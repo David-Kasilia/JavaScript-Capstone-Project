063e0e555c72f14056ae12ec3bffe4ca52da2080
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-31 22:56:01 +0430

    Adds comments pop-up

--- src/modules/content.js
+++ src/modules/content.js
@@ -1,4 +1,7 @@
 // populating the html container for the pokemon
+import showModal from './comments.js';
+
+const modal = document.getElementById('modal');
 
 const createPokemonCard = (pokemon) => {
   const pokeContainer = document.getElementById('poke-container');
@@ -7,19 +10,28 @@ const createPokemonCard = (pokemon) => {
 
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
+  const comments = document.createElement('button');
+  comments.classList.add('comments-btn');
+  comments.innerText = 'Comments';
+  comments.setAttribute('id', 'comments');
+  comments.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    showModal(pokemon);
+  });
+
   const pokeInnerHTML = `
-  <div class="img-container">
+  <div class="${pokemon.id} img-container">
   <img class="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png">
   </div>
 <div class="info">
 <h3 class="poke-name">${name} <span class="material-symbols-outlined">
 favorite
 </span> </h3>
-<button id="comments" class="comments-btn">Comments</buttons>
 </div>
   `;
 
   pokemonEl.innerHTML = pokeInnerHTML;
+  pokemonEl.append(comments);
 
   pokeContainer.appendChild(pokemonEl);
 };
